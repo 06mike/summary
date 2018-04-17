3008b835aa228ac85c17a9c49002b3eb2ab75aef
binary <06mike.h@163.com>
2018-04-17 16:46:50 +0800

    init

---
+++ src/main/java/com/org/my/Demo.java
@@ -0,0 +1,16 @@
+package com.org.my;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by binary on 18/4/17.
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("hello java");
+        List<String> list = Arrays.asList("a","b","c");
+    }
+
+}
